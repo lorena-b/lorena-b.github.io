c1caf069fd9e6b7dad35a85eb32e4860919a6d3a
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-12-28 20:56:32 -0500

    react

--- web-portfolio/src/components/Header.js
+++ web-portfolio/src/components/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Header extends Component {
-  render() {
-    return (
-      <div>
-
-
-        
-      </div>
-    )
-  }
+    render() {
+        return ( 
+            <div>
+                <header class="header">
+                    <h1>LORENA BUCIU</h1>
+                </header>
+            </div>
+        )
+    }
 }
