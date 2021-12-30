4628e91e029d732050070c8d8d061769153cfad4
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-12-30 01:36:09 -0500

    Update Home.js

--- web-portfolio/src/components/screens/Home.js
+++ web-portfolio/src/components/screens/Home.js
@@ -9,24 +9,24 @@ export default class Home extends Component {
     return (
       <div>
         <Helmet>
-          <title>{ TITLE }</title>
+          <title>{TITLE}</title>
         </Helmet>
         <main id="main">
-        <div className="container">
-          <div id="avatar">
-            <img src={av} alt="avatar" />
+          <div className="container">
+            <div id="avatar">
+              <img src={av} alt="avatar" />
+            </div>
+            {/* Text Info */}
+            <div className="info">
+              <p></p>
+              <h2 id="info-head">Hi, I'm Lorena!</h2>
+              <h2 id="emoji"> 👋</h2>
+              <br /><br /> I am experienced in both front-end and back-end development.
+              I am proficient in <code>Python</code> and <code>Java</code>. CONTINUE...
+              <p />
+            </div>
           </div>
-          {/* Text Info */}
-          <div className="info">
-            <p></p>
-            <h2 id="info-head">Hi, I'm Lorena!</h2>
-            <h2 id="emoji"> 👋</h2>
-            <br /><br /> I am experienced in both front-end and back-end development. 
-            I am proficient in <code>Python</code> and <code>Java</code>. CONTINUE...
-            <p />
-          </div>
-        </div>
-       </main>        
+        </main>
       </div>
     )
   }
