e363b264584764311daae6a4035f8350edb7f2b0
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-12-30 01:56:13 -0500

    Update Resume.js

--- web-portfolio/src/components/screens/Resume.js
+++ web-portfolio/src/components/screens/Resume.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
 
 const TITLE = "Resume"
 
-export default class Resume extends Component {
-    render() {
-        return (
-            <div>
-                <Helmet>
-                    <title>{TITLE}</title>
-                </Helmet>
-                <main id="main">
-                    <div className="container">
-                        {/* Resume Info */}
-                        <div className="info">
-                            <iframe src="https://drive.google.com/file/d/17MGMyTKIm5eKPO6lxcFJv8W3SS1q1iFu/preview" width="640" height="480" allow="autoplay" title="resume"></iframe>                        </div>
+function Resume() {
+    const [embedURL] = useState("https://drive.google.com/file/d/17MGMyTKIm5eKPO6lxcFJv8W3SS1q1iFu/preview")
+    return (
+        <div>
+            <Helmet>
+                <title>{TITLE}</title>
+            </Helmet>
+            <main id="main">
+                <div className="container">
+                    {/* Resume Info */}
+                    <div className="info">
+                        <iframe src={embedURL} width="640" height="480" allow="autoplay" title="resume"></iframe>
                     </div>
-                </main>
-
-            </div>
-        )
-    }
+                </div>
+            </main>
+        </div>
+    )
 }
+
+export default Resume;
