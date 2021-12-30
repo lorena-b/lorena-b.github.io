6cc2f5200b144e7b32984c6e92061411ccc1ff86
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2021-12-30 16:36:19 -0500

    start animate

--- web-portfolio/src/components/screens/Home.js
+++ web-portfolio/src/components/screens/Home.js
@@ -2,32 +2,32 @@ import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import av from './img/pfp.png'
 
-const TITLE = "Home"
+const TITLE = "About"
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <Helmet>
-          <title>{TITLE}</title>
-        </Helmet>
-        <main id="main">
-          <div className="container">
-            <div id="avatar">
-              <img src={av} alt="avatar" />
+        <div>
+          <Helmet>
+            <title>{TITLE}</title>
+          </Helmet>
+          <main id="main">
+            <div className="container">
+              <div id="avatar">
+                <img src={av} alt="avatar" />
+              </div>
+              {/* Text Info */}
+              <div className="info">
+                <p></p>
+                <h2 id="info-head">Hi, I'm Lorena!</h2>
+                <h2 id="emoji"> 👋</h2>
+                <br /><br /> I am experienced in both front-end and back-end development.
+                I am proficient in <code>Python</code> and <code>Java</code>. CONTINUE...
+                <p />
+              </div>
             </div>
-            {/* Text Info */}
-            <div className="info">
-              <p></p>
-              <h2 id="info-head">Hi, I'm Lorena!</h2>
-              <h2 id="emoji"> 👋</h2>
-              <br /><br /> I am experienced in both front-end and back-end development.
-              I am proficient in <code>Python</code> and <code>Java</code>. CONTINUE...
-              <p />
-            </div>
-          </div>
-        </main>
-      </div>
+          </main>
+        </div>
     )
   }
 }
