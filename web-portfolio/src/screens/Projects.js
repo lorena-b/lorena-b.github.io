d22213e94c9d5fa94d44ce503240c52a37cb852c
Lorena Buciu <lorena.buciu@mail.utoronto.ca>
2022-01-02 18:30:35 -0500

    projects info

--- web-portfolio/src/screens/Projects.js
+++ web-portfolio/src/screens/Projects.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import AnimatedPage from '../components/AnimatedPage'
 import testimg from "../assets/img/projimg.png"
+import redditgif from "../assets/img/demogif.gif"
+import vslr from "../assets/img/frontend.png"
 
 const TITLE = "Projects"
 
@@ -21,10 +23,12 @@ export default class Projects extends Component {
                   <h2>UofT Course Scheduling App</h2>
                   <a href="https://github.com/lorena-b/UofT-Course-Scheduling-Application"><img className="proj-img hover-glow" src={testimg} alt="proj1"/></a>
                   <p className="proj-info">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book.
+                  A user specifies which courses they want to take, and also specify filters, like "No classes after 5 PM", or to find sections with no conflicts, 
+                  through the CLI. The program queries the U of T Academic Calendar 
+                  for the Tutorial & Lecture Sections of each requested course, and creates all schedules that 
+                  meet the given criteria. This can be navigated either directly in the CLI. The user selects a 
+                  generated schedule, and the program generates .ics/.csv/.jpg files for different purposes, such as 
+                  for calendar apps.
                   </p>
                   <div className="code-tags">
                     <button className="tag"> Java </button>
@@ -32,12 +36,10 @@ export default class Projects extends Component {
                 </div>
                 <div className="project">
                   <h2>Reddit Topic Analysis</h2>
-                  <a href="https://github.com/lorena-b/TreeHacks"><img className="proj-img hover-glow" src={testimg} alt="proj2"/></a>
+                  <a href="https://github.com/lorena-b/TreeHacks"><img className="proj-img hover-glow" src={redditgif} alt="proj2"/></a>
                   <p className="proj-info">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book.
+                  The program asks the user to input a specific topic or keyword. It then searches through about 1,000 Reddit posts with that corresponding keyword. It then outputs the market popularity of that topic. It also outputs consumer sentiment towards that topic,
+                   from negative to positive expressed as a percentage. Used Flask to establish a back-end server and API to receive user input then use our processing algorithms to return the relevant data. The PRAW reddit API wrapper for python allowed us to retrieve post data from reddit.
                   </p>
                   <div className="code-tags">
                     <button className="tag"> Python </button>
@@ -47,12 +49,11 @@ export default class Projects extends Component {
                 </div>
                 <div className="project">
                   <h2>VSLR Model</h2>
-                  <a href="https://github.com/lorena-b/VSLR-model"><img className="proj-img hover-glow" src={testimg} alt="proj3"/></a>
+                  <a href="https://github.com/lorena-b/VSLR-model"><img className="proj-img hover-glow" src={vslr} alt="proj3"/></a>
                   <p className="proj-info">
-                  Lorem Ipsum is simply dummy text of the printing and typesetting
-                  industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book.
+                  Using Vancouver sea level rise CSV data, this program extrapolates the predicted sea level rise over the years using a combination of 
+                  SARIMAX and Theil-Sen regression models. Using Dash and Plotly capabilities these values are plotted onto a map of Vancouver with an interactive 
+                  slider to visualize which areas in the Vancouver region are most susceptible to flooding 280 years into the future. 
                   </p>
                   <div className="code-tags">
                     <button className="tag"> Python </button>
